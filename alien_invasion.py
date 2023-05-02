76adf9a9469a4a89aa361c17001c80d31b2802cb
flyajar <rj.jovenal@gmail.com>
2023-05-02 21:45:56 +0800

    added ship to the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -9,8 +10,10 @@ class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
+
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
+        self.ship = Ship(self)
 
         self.bg_color = (230, 230, 230)
 
@@ -21,6 +24,7 @@ class AlienInvasion:
                     sys.exit()
 
                 self.screen.fill(self.settings.bg_color)
+                self.ship.blitme()
                 pygame.display.flip()
 
 
