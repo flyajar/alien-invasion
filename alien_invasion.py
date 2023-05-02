95a2d017b339b72e22920e8bc7ec83f5179b3626
flyajar <rj.jovenal@gmail.com>
2023-05-02 20:16:33 +0800

    added settings and image for ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,13 +1,14 @@
 import sys
-
 import pygame
+
+from settings import Settings
 
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
-
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
 
         self.bg_color = (230, 230, 230)
@@ -18,11 +19,10 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-                self.screen.fill(self.bg_color)
+                self.screen.fill(self.settings.bg_color)
                 pygame.display.flip()
 
 
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
-
